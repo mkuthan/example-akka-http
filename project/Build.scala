675205e8d73a771dcfae7d0cc0d0fa1b29555874
Marcin Kuthan <marcin.kuthan@allegrogroup.com>
2014-12-04 22:20:28 +0100

    Extract project settings into separate object.

--- project/Build.scala
+++ project/Build.scala
@@ -18,40 +18,44 @@ import org.scalastyle.sbt.ScalastylePlugin
 import sbt.Keys._
 import sbt._
 
+object BuildSettings {
+
+  val akkaV = "2.3.7"
+  val sprayV = "1.3.2"
+  val specsV = "2.4.14"
+
+  val buildSettings = Defaults.coreDefaultSettings ++
+    Seq(
+      organization := "http://mkuthan.github.io/",
+      version := "1.0",
+      scalaVersion := "2.11.4",
+      scalacOptions := Seq(
+        "-deprecation",
+        "-encoding", "UTF-8",
+        "-feature",
+        "-unchecked"),
+      resolvers ++= Seq(
+        Classpaths.sbtPluginReleases,
+        Classpaths.typesafeReleases,
+        "Sonatype OSS Releases" at "http://oss.sonatype.org/content/repositories/releases/",
+        "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases"
+      ),
+      libraryDependencies ++= Seq(
+        "io.spray" %% "spray-can" % sprayV,
+        "io.spray" %% "spray-routing" % sprayV,
+        "io.spray" %% "spray-testkit" % sprayV % "test",
+        "com.typesafe.akka" %% "akka-actor" % akkaV,
+        "com.typesafe.akka" %% "akka-testkit" % akkaV % "test",
+        "org.specs2" %% "specs2" % specsV % "test"
+      )
+    ) ++ ScalastylePlugin.Settings
+}
+
 object ApplicationBuild extends Build {
 
+  import BuildSettings._
+
   lazy val main = Project(id = "example-spray", base = file("."), settings = buildSettings)
-
-  def buildSettings = {
-
-    val akkaV = "2.3.7"
-    val sprayV = "1.3.2"
-    val specsV = "2.4.14"
-
-    Defaults.coreDefaultSettings ++
-      Seq(
-        scalaVersion := "2.11.4",
-        scalacOptions := Seq(
-          "-deprecation",
-          "-encoding", "UTF-8",
-          "-feature",
-          "-unchecked"),
-        resolvers ++= Seq(
-          Classpaths.sbtPluginReleases,
-          "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
-          "Sonatype OSS Releases" at "http://oss.sonatype.org/content/repositories/releases/",
-          "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases"
-        ),
-        libraryDependencies ++= Seq(
-          "io.spray" %% "spray-can" % sprayV,
-          "io.spray" %% "spray-routing" % sprayV,
-          "io.spray" %% "spray-testkit" % sprayV % "test",
-          "com.typesafe.akka" %% "akka-actor" % akkaV,
-          "com.typesafe.akka" %% "akka-testkit" % akkaV % "test",
-          "org.specs2" %% "specs2" % specsV % "test"
-        )
-      ) ++ ScalastylePlugin.Settings
-  }
 
 }
 
